7e74f6c4c2bd5ef9dc9f8bb7d2433cd57596723e
yaxin <yxin>
2023-12-17 00:11:22 +0100

    1

--- Model/UI/uploadFile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RightControl.Model
-{
-    public class uploadFile
-    {
-        public int code { get; set; }
-        public string msg { get; set; }
-        public string src { get; set; }
-    }
-}
